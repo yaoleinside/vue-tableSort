9da72d564665e9fa6c94e576f9ff7f462e03f027
yaole <yaole_yl@qq.com>
2017-03-21 15:18:36 +0800

    done

--- main.js
+++ main.js
@@ -3,54 +3,54 @@
  */
 
 Vue.component('demo-grid', {
-    template: "#grid-template",
+    template: '#grid-template',
     props: {
         data: Array,
         columns: Array,
         filterKey: String
     },
     data: function () {
-        var sortOrders = {};
+        var sortOrders = {}
         this.columns.forEach(function (key) {
-            sortOrders[key] = 1;
+            sortOrders[key] = 1
         });
         return {
             sortKey: '',
             sortOrders: sortOrders
         }
-    }
+    },
     computed: {
         filteredData: function () {
-            var sortKey = this.sortKey;
-            var order = this.sortOrders[sortKey] || 1;
-            var filterKey = this.filterKey && this.filterKey.toLowerCase();
-            var data = this.data;
+            var sortKey = this.sortKey
+            var filterKey = this.filterKey && this.filterKey.toLowerCase()
+            var order = this.sortOrders[sortKey] || 1
+            var data = this.data
             if (filterKey) {
-                data = data.filter(function (data) {
-                    return Object.keys(data).some(function (key) {
-                        return String(data[key]).toLowerCase().indexOf(filterKey) > -1;
+                data = data.filter(function (row) {
+                    return Object.keys(row).some(function (key) {
+                        return String(row[key]).toLowerCase().indexOf(filterKey) > -1
                     })
                 })
             }
             if (sortKey) {
                 data = data.slice().sort(function (a, b) {
-                    a = a[sortKey];
-                    b = b[sortKey];
-                    return (a === b ? 0 : a > b ? 1 : -1) * order;
+                    a = a[sortKey]
+                    b = b[sortKey]
+                    return (a === b ? 0 : a > b ? 1 : -1) * order
                 })
             }
-            return data;
+            return data
         }
     },
     filters: {
         capitalize: function (str) {
-            return str[0].toUpperCase() + str.slice(1);
+            return str.charAt(0).toUpperCase() + str.slice(1)
         }
     },
     methods: {
         sortBy: function (key) {
-            this.sortKey = key;
-            this.sortOrders[key] = this.sortOrders[key] * -1;
+            this.sortKey = key
+            this.sortOrders[key] = this.sortOrders[key] * -1
         }
     }
 })
@@ -61,10 +61,10 @@ var demo = new Vue({
         searchQuery: '',
         gridColumns: ['name', 'power'],
         gridData: [
-            {name: 'Chuck Norris', power: Infinity},
-            {name: 'Bruce Lee', power: 9000},
-            {name: 'Jackie Chan', power: 7000},
-            {name: 'Jet Li', power: 8000}
+            { name: 'Chuck Norris', power: Infinity },
+            { name: 'Bruce Lee', power: 9000 },
+            { name: 'Jackie Chan', power: 7000 },
+            { name: 'Jet Li', power: 8000 }
         ]
     }
 })
